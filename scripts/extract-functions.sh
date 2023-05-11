a9079d86804f513a47535a492a1cbe09fae71e86
mskordal <skordalakis@protonmail.com>
2023-05-11 12:22:28 +0100

    main gets the correct id=1, other functions get >=2

--- scripts/extract-functions.sh
+++ scripts/extract-functions.sh
@@ -5,7 +5,8 @@ output_file="functions.tx"
 ctags -o $output_file --fields=+n --c-kinds=f $1 
 # print only the first column except when starting with ! and add a number next
 # to it starting at 1
-awk '!/^!/ {print $1, ++count}' $output_file > $output_file"t"
+awk '!/^!/ {if($1 == "main"){print $1, 1} else {print $1, ++count + 1}}' $output_file > $output_file"t"
+# vim +'norm GddggPZZ' $output_file"t" 
 rm $output_file
 
 
