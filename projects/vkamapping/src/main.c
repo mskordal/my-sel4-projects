eda903a860e9a7cba08ad085213720770a67477a
mskordal <skordalakis@protonmail.com>
2023-06-28 13:57:13 +0100

    CMakeLists.txt: Changed to add the pass to the main file. -- src/main.c: added dummy function calls borrowed from gfc.c to test the pass. Tidied up the code as well

--- projects/vkamapping/src/main.c
+++ projects/vkamapping/src/main.c
@@ -20,6 +20,11 @@
 #include <sel4utils/process.h>
 
 
+
+#define HLS_BASE_ADDR				0xa0000000
+#define HLS_FUNCTION_COUNTER_OFFSET	0x10
+#define HLS_BRAM_ADDR_OFFSET		0x18
+
 #define BRAM_BASE_ADDR	0xa0010000
 #define BRAM_SIZE_BITS 	13 // 2^16 = 8 KBytes
 #define TEST_VADDR		0x7000000 // this virtual address works
@@ -49,6 +54,19 @@ UNUSED static sel4utils_alloc_data_t data;
 #define THREAD_2_STACK_SIZE 4096
 UNUSED static int thread_2_stack[THREAD_2_STACK_SIZE];
 
+
+static void D() { }
+static void Y() { D(); }
+static void X() { Y(); }
+static void C() { D(); X(); }
+static void B() { C(); }
+static void S() { D(); }
+static void P() { S(); }
+static void O() { P(); }
+static void N() { O(); }
+static void M() { N(); }
+static void G() { M(); }
+static void A() { B(); G(); }
 
 int main(void)
 {
@@ -89,7 +107,8 @@ int main(void)
 	UNUSED reservation_t virtual_reservation;
 	virtual_reservation = vspace_reserve_range(&vspace,
 		ALLOCATOR_VIRTUAL_POOL_SIZE, seL4_AllRights, 1, &vaddr);
-	ZF_LOGF_IF(virtual_reservation.res == NULL, "Failed to reserve a chunk of memory.\n");
+	ZF_LOGF_IF(virtual_reservation.res == NULL,
+		"Failed to reserve a chunk of memory.\n");
 	
 	bootstrap_configure_virtual_pool(allocman, vaddr,
 		ALLOCATOR_VIRTUAL_POOL_SIZE, simple_get_pd(&simple));
@@ -98,14 +117,16 @@ int main(void)
 	/* use sel4utils to make a new process */
 	sel4utils_process_t new_process;
 
-	sel4utils_process_config_t config = process_config_default_simple(&simple, APP_IMAGE_NAME, APP_PRIORITY);
-	error = sel4utils_configure_process_custom(&new_process, &vka, &vspace, config);
+	sel4utils_process_config_t config = process_config_default_simple(&simple,
+		APP_IMAGE_NAME, APP_PRIORITY);
+	error = sel4utils_configure_process_custom(&new_process, &vka, &vspace,
+		config);
 	ZF_LOGF_IFERR(error, "Failed to spawn a new thread.\n");
 
 	/* give the new process's thread a name */
 	NAME_THREAD(new_process.thread.tcb.cptr, "dynamic-3: process_2");
 
-	/**
+/**
 	 * create arguments to pass to the process when it is spawned. Here we pass
 	 * one argument of value 5
 	 */
@@ -115,18 +136,19 @@ int main(void)
 	sel4utils_create_word_args(string_args, argv, argc, 5);
 
 	/* spawn the process */
-	error = sel4utils_spawn_process_v(&new_process, &vka, &vspace, argc, (char**) &argv, 1);
+	error = sel4utils_spawn_process_v(&new_process, &vka, &vspace, argc,
+		(char**) &argv, 1);
 	ZF_LOGF_IFERR(error, "Failed to spawn and start the new thread.\n");
 
-
 	vka_object_t bram_frame_object;
+
 	/**
 	 * vka_alloc_frame is used in the tutorial. We call this function which is
 	 * called 2 levels internally since here we can specify the physical
 	 * address we want to map and that we want to use a device
 	 */
-	vka_alloc_object_at_maybe_dev(	&vka, seL4_ARM_SmallPageObject, BRAM_SIZE_BITS,
-									BRAM_BASE_ADDR, MAP_A_DEVICE, &bram_frame_object);
+	vka_alloc_object_at_maybe_dev(	&vka, seL4_ARM_SmallPageObject,
+		BRAM_SIZE_BITS, HLS_BASE_ADDR, MAP_A_DEVICE, &bram_frame_object);
 
 	ZF_LOGF_IFERR(error, "Failed to alloc a frame in BRAM.\n");
 
@@ -134,17 +156,17 @@ int main(void)
 	// will populate it with reference to all additional objects that were
 	// created in order to map the page. num_objects will be updated with the
 	// actual number of objects that will be allocated
-
 	vka_object_t objects[2];
 	int num_objects = 2;
 
 	seL4_Word *x = (seL4_Word *) TEST_VADDR;
 	error = sel4utils_map_page(	&vka, pd_cap, bram_frame_object.cptr, (void*)x,
-								seL4_ReadWrite, 0, objects, &num_objects);
-	ZF_LOGF_IFERR(error, "Failed again to map the bram frame into the VSpace.\n");
+		seL4_ReadWrite, 0, objects, &num_objects);
+	ZF_LOGF_IFERR(error,
+		"Failed again to map the bram frame into the VSpace.\n");
 
-	printf("Set x to 5\n");
-	*x = 5;
-	printf("Read x: %lu\n", *x);
+	A();
+	while(1);
+
 	return 0;
 }
