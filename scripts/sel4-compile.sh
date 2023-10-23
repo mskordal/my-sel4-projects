285826686a73a3a586626bc661456c315e7844a1
mskordal <skordalakis@protonmail.com>
2023-10-23 18:18:53 +0100

    enabled llvm symbolizer on sel4 compilation script

--- scripts/sel4-compile.sh
+++ scripts/sel4-compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export LLVM_DIR=/usr/lib/llvm-15
+export LLVM_SYMBOLIZER_PATH=$LLVM_DIR/bin/llvm-symbolizer
 rm -rf ./*
 ../init-build.sh -DPLATFORM=zcu102 -DTRIPLE=aarch64-linux-gnu
 ninja
