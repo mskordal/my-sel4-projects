e051c7d43e77e8260275bd33579a5ca573dd58ec
mskordal <skordalakis@protonmail.com>
2023-10-23 18:51:03 +0100

    made pass more generic to create declaration of functions when required. Updated gfc to use basic defines

--- myLlvmPass/gfc.c
+++ myLlvmPass/gfc.c
@@ -1,16 +1,23 @@
-static void D() { }
-static void Y() { D(); }
-static void X() { Y(); }
-static void C() { D(); X(); }
-static void B() { C(); }
-static void S() { D(); }
-static void P() { S(); }
-static void O() { P(); }
-static void N() { O(); }
-static void M() { N(); }
-static void G() { M(); }
-static void A() { B(); G(); }
+#define REPEAT 10000000  //9999999999
 
-int main() {
+#define VARS unsigned long i,j;
+
+#define CALC(x) for(i=0; i<REPEAT/x; i++) {++j;}
+
+static void D() {VARS CALC(1)}
+static void Y() {VARS CALC(1) D(); CALC(3)}
+static void X() {VARS CALC(1) Y(); CALC(1)}
+
+static void C() {VARS CALC(3) D(); CALC(3) X(); CALC(9)}
+static void B() {VARS CALC(5) C(); CALC(7) }
+static void S() {VARS CALC(2) D(); CALC(9) }
+static void P() {VARS CALC(8) S(); CALC(3) }
+static void O() {VARS CALC(3) P(); CALC(1) }
+static void N() {VARS CALC(2) O(); CALC(6) }
+static void M() {VARS CALC(7) N(); CALC(8) }
+static void G() {VARS CALC(3) M(); CALC(4) }
+static void A() {VARS CALC(7) B(); CALC(2) G(); CALC(6)}
+int main(void)
+{
   A();
 }
